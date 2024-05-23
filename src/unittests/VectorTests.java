de25c5a3323054cef330ba2c9dabc7b867e8f105
Tal <tal.allon2020@gmail.com>
2024-05-23 13:52:37 +0300

    commit

--- src/unittests/VectorTests.java
+++ src/unittests/VectorTests.java
@@ -32,7 +32,7 @@ class VectorTests {
 	        // =============== Boundary Values Tests ==================
 			 // TC11: test zero vector from constructor 
 			 assertThrows(IllegalArgumentException.class, () -> vector.equals(Double3.ZERO), //
-			 "crossProduct() for parallel vectors does not throw an exception");
+			 "ConstructorWithDouble3()  vector does not throw an exception");
 	    }
 
 	    @Test
@@ -52,7 +52,7 @@ class VectorTests {
 	     // =============== Boundary Values Tests ==================
 			 // TC11: test zero vector from constructor 
 			 assertThrows(IllegalArgumentException.class, () -> vector.equals(Double3.ZERO), //
-			 "crossProduct() for parallel vectors does not throw an exception");
+			 "ConstructorWithComponents()  vector does not throw an exception");
 	    }
 
 	/**
@@ -67,15 +67,16 @@ class VectorTests {
         
         Vector expected1 = new Vector(0,0,0);
         Vector expected = new Vector(3, 6, 9);
+
 		 // TC01: Test that add vector to vector and check that the result is proper.
 
         assertEquals(expected, v1.add(v2),"wrong add vector");
-        assertEquals(expected, v3.add(v2),"wrong add vector");
+        assertEquals(v1, v3.add(v2),"wrong add vector");
 
      // =============== Boundary Values Tests ==================
 		 // TC11: test zero result from add  opposite and equals vectors 
-		 assertThrows(IllegalArgumentException.class, () ->v1.add(v3).equals(expected1), //
-		 "crossProduct() for parallel vectors does not throw an exception");
+		 assertThrows(IllegalArgumentException.class, () ->v1.add(v3), //
+		 "addVector() for parallel vectors does not throw an exception");
 	
 	
 	}
@@ -89,17 +90,19 @@ class VectorTests {
 				Vector v = new Vector(1, 2, 3);
 		        int scalar = 2;
 		        int scalar1 = -2;
+		        int scalar2 = 0;
+
 		        Vector expected = new Vector(2, 4, 6);
 		        Vector expected1 = new Vector(-2, -4,- 6);
 		        Vector v2 = new Vector(0, 0, 0);
-				Vector zeroScaledVector = v2.scale(scalar);
 				 // TC01: Test that multiply a vector by a scalar and check that the result is correct.
 		        assertEquals(expected, v.scale(scalar), "wrong scaled vector");
-		        // TC10: Test that multiply a vector by a scalar and check that the result is correct.
+		        // TC10: Test that multiply a vector by a negative scalar and check that the result is correct.
 		        assertEquals(expected1, v.scale(scalar1), "wrong scaled vector");
 		     // =============== Boundary Values Tests ==================
 				// TC11: Test for scaling a zero vector
-				assertEquals(v2, zeroScaledVector, "scaling a zero vector should result in a zero vector");
+		        assertThrows(IllegalArgumentException.class, () ->v.scale(scalar2), //
+		       		 "scale() with scalar 0 does not throw an exception");
 			}
 
 	/**
@@ -109,21 +112,23 @@ class VectorTests {
 	void testDotProduct() {
 		// ============Equivalence Partitions Tests ==============
 		Vector v1 = new Vector(1, 2, 3);
-        Vector v2 = new Vector(2, 4, 6);
-        Vector v3 = new Vector(-2,- 4, -6);
-		Vector v4 = new Vector(1, 1, 1);
+        Vector v2 = new Vector(2, -3, 6);
+		Vector v4 = new Vector(1, 0, 0);
+		Vector v5 = new Vector(-1,-1, 1);
+        
 
-        double expected = 28;
+        double expected = 14;
         double expected1 = 0;
         double expected2 = 6;
 
         //TC01: Checks the correctness and result of the inner multiplication operation (dot product) between two vectors
         assertEquals(expected, v1.dotProduct(v2), "wrong dot product value");
-      //TC10: Checks the correctness and result of the vertical multiplication operation (dot product) between two vectors
-        assertEquals(expected1, v2.dotProduct(v3), "wrong dot product value");
+      
+     // =============== Boundary Values Tests ==================
       //TC11: Checks the correctness and result of the A unit vector with a different vector multiplication operation (dot product) between two vectors
         assertEquals(expected2, v1.dotProduct(v4), "wrong dot product value");
-	
+      //TC11: Checks the correctness and result of the A vertical vector with a different vector multiplication operation (dot product) between two vectors
+        assertEquals(expected1, v1.dotProduct(v5), "wrong dot product value");
 	}
 
 	/**
@@ -134,7 +139,7 @@ class VectorTests {
 		 Vector v1         = new Vector(1, 2, 3);
 	     Vector v2         = new Vector(-2, -4, -6);
 	     Vector v3         = new Vector(0, 3, -2);
-		
+	     Vector v4         = new Vector(2, 4, 6);
 		 // ============ Equivalence Partitions Tests ==============
 		 Vector vr = v1.crossProduct(v3);
 		 // TC01: Test that length of cross-product is proper (orthogonal vectors taken
@@ -143,10 +148,20 @@ class VectorTests {
 		 // TC02: Test cross-product result orthogonality to its operands
 		 assertEquals(0, vr.dotProduct(v1), "crossProduct() result is not orthogonal to 1st operand");
 		 assertEquals(0, vr.dotProduct(v3), "crossProduct() result is not orthogonal to 2nd operand");
-		 // =============== Boundary Values Tests ==================
-		 // TC11: test zero vector from cross-product of parallel vectors
-		 assertThrows(IllegalArgumentException.class, () -> v1.crossProduct(v2), //
-		 "crossProduct() for parallel vectors does not throw an exception");
+		 
+		// =============== Boundary Values Tests ==================
+			// TC10: Test for the inner multiplication operation (dot product) between two vectors with same directions.
+	        assertThrows(IllegalArgumentException.class, () ->v1.crossProduct(v4), 
+	       		 "crossProduct() with same directions does not throw an exception");
+	     // TC11: Test for the inner multiplication operation (dot product) between two vectors with opposite directions and equals.
+	        assertThrows(IllegalArgumentException.class, () ->v4.crossProduct(v2), 
+	       		 "crossProduct() with same directions does not throw an exception");
+	     // TC100: Test for the inner multiplication operation (dot product) between two vectors with opposite directions.
+	        assertThrows(IllegalArgumentException.class, () ->v1.crossProduct(v2), 
+	       		 "crossProduct() with same directions does not throw an exception");
+	     // TC101: Test for the inner multiplication operation (dot product) between two same vectors.
+	        assertThrows(IllegalArgumentException.class, () ->v1.crossProduct(v1), 
+	       		 "crossProduct() with same directions does not throw an exception");
 		 }
 
 	/**
@@ -191,10 +206,10 @@ class VectorTests {
 	//TC01: Simple test
 		assertEquals(1d,n.lengthSquared(),0.00001,"wrong normalized vector length");
 		assertThrows(IllegalArgumentException.class,()->v.crossProduct(n),"normalized vector is not in the same direction");
-		assertEquals(new Vector (0,0.,0.8),n,"wrong normalized vector");
+		assertEquals(new Vector (0,0.6,0.8),n,"wrong normalized vector");
 		// =============== Boundary Values Tests ==================
 				// TC02: Test for a vector which is already a unit vector
-				Vector unitVector = new Vector(1 / Math.sqrt(14), Math.sqrt(2 / 7), 2 / Math.sqrt(14));
+				Vector unitVector = new Vector(1,0,0);
 				Vector nUnitVector = unitVector.normalize();
 				assertEquals(unitVector, nUnitVector, "normalized vector is not the same as the original unit vector");
 	
