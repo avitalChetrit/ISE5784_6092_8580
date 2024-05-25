e14f475cf5d8651246712c913a50699dc1251b2a
avitalChetrit <154763373+avitalChetrit@users.noreply.github.com>
2024-05-25 21:50:14 +0300

    Update Tube.java

--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -28,31 +28,6 @@ public class Tube extends RadialGeometry {
     }
     
     /**
-     * Returns the direction vector of the tube's axis.
-     *
-     * @return The direction vector of the tube's axis
-     */
-    public Vector getDirection() {
-        return axisRay.getDirection();
-    }
-    /**
-     * Returns the ray representing the axis of the tube.
-     *
-     * @return The ray representing the axis of the tube
-     */
-    /**
-     * Aligns a double value to zero if it is very close to zero.
-     * @param val The double value to align
-     * @return The aligned value
-     */
-    private double alignZero(double val) {
-        final double EPSILON = 0.0000001;
-        return (Math.abs(val) < EPSILON) ? 0.0 : val;
-    }
-    public Ray getRay() {
-        return axisRay;
-    }
-    /**
      * Returns the normal vector to the surface of the tube at a given point.
      *
      * @param point The point on the surface of the tube
@@ -60,23 +35,15 @@ public class Tube extends RadialGeometry {
      */
     @Override
     public Vector getNormal(Point point) {
-        // Calculate the vector from the head of the axis to the given point 
-        Vector v = point.subtract(axisRay.getHead());
         
         // Calculate the parameter t along the axis direction
-        double t = alignZero(axisRay.getDirection().dotProduct(v));
-       
-        // Check if the point is on the axis of the cylinder
-        if (t == 0) {
-            // If the point is on the axis of the cylinder, return the normalized vector v
-            return v.normalize();
-        }
+        double t = axisRay.getDirection().dotProduct(point.subtract(axisRay.getHead()));
         
-        // Calculate the closest point 'p' on the axis to the given point
-        Point p = axisRay.getPoint(t);
+        // Calculate the closest point 'O' on the axis to the given point
+        Point O = axisRay.getHead().add(axisRay.getDirection().scale(t));
 
         // Return the normalized vector from the closest point on the axis to the given point
-        return point.subtract(p).normalize();   
+        return point.subtract(O).normalize();   
     }
    
 }
